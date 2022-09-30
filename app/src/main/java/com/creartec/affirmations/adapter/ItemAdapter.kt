3551728efc4a3e6cc5c00ba6261af77bbab575d6
jhordanluyo <jhordan271992@gmail.com>
2022-09-29 23:19:45 -0500

    ejemplo recycler view terminado

--- app/src/main/java/com/creartec/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/creartec/affirmations/adapter/ItemAdapter.kt
@@ -4,16 +4,19 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.creartec.affirmations.R
 import com.creartec.affirmations.model.Affirmation
 
-class ItemAdapter (
+class ItemAdapter(
     private val context: Context,
-    private val dataset:List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
-    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -25,7 +28,10 @@ class ItemAdapter (
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.textView.setText(item.stringResourceId)
+        //holder.textView.text = context.resources.getString(item.stringResourceId)
+        /* puede usarse dos formas para presentar un elemento de la lista */
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount() = dataset.size
